af7141255ca07cdba481843ca2bd1efea99c69d8
kefasjw <11681703+kefasjw@users.noreply.github.com>
2024-05-28 08:00:15 +0700

    sdas

--- lib/src/utils/delay.dart
+++
@@ -1,7 +0,0 @@
-Future<void> delay(bool addDelay, [int milliseconds = 2000]) {
-  if (addDelay) {
-    return Future.delayed(Duration(milliseconds: milliseconds));
-  } else {
-    return Future.value();
-  }
-}
